3c50133834c6d64193f7f19ccb793b2b18e5a188
psli0101 <p.s.li19980101@gmail.com>
2019-07-29 17:13:16 +0800

    update

--- src/containers/Example.js
+++ src/containers/Example.js
@@ -88,17 +88,17 @@ class Example extends Component {
       .then((statResult) => {
         if (statResult[0].isFile()) {
           // if we have a file, read it
-          return RNFS.readFile(statResult[1], 'utf8');
+          alert(RNFS.readFile(statResult[1], 'utf8'));
         }
 
         return 'no file';
       })
       .then((contents) => {
         // log the file contents
-        console.log(contents);
+        alert(contents);
       })
       .catch((err) => {
-        console.log(err.message, err.code);
+        alert(err.message, err.code);
       });
   }
 
