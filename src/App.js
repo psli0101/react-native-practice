b4b9b9b6b8955428ee59bd1d6df6bf4d9958ed32
P.S. Li <p.s.li19980101@gmail.com>
2019-08-12 16:23:53 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react';
-import { Provider } from 'react-redux';
 import { createAppContainer } from 'react-navigation';
 
 import { AppNavigator } from '@src/router/router';
-import store from './redux/store';
 
 import { pushNotifications } from '@services';
 
@@ -14,9 +12,7 @@ const Route = createAppContainer(AppNavigator);
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Route/>
-      </Provider>
+      <Route/>
     );
   }
 }
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,2 +1,1 @@
-export * from './toDoList/getList/actions';
-export * from './toDoList/auth/actions';
+
